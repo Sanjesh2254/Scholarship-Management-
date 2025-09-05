afd08523b2e0eb67f7361ff833b54862a96003ca
Sanjesh2254 <rsanjesh64@gmail.com>
2025-09-05 10:19:34 +0530

    Delete unwanted doctypes

--- scholarship_management/scholarship_management/doctype/sample/sample.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, sanjesh and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class sample(Document):
-	pass
--- scholarship_management/scholarship_management/doctype/sample/test_sample.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, sanjesh and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testsample(FrappeTestCase):
-	pass
